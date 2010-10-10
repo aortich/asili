76ced8d9fa24664f81e20008f7f335c04e2ee9da
t226yo <t226yo@15ee1b71-846b-a757-c8fc-a91a176c5e57>
2010-10-10 21:39:37 +0000

    Ya se completaron los sprites de las opciónes del menú principal, hace falta ponerle sus respectivas acciones a tomar, fueron probados con un system out que puede ser borrado posteriormente
    Se recortaron los sprites de las imágenes para que fueran acomodadas  de manera óptima. nueva resolución, 360x50 px

--- Asili/src/SpriteTrama.java
+++ Asili/src/SpriteTrama.java
@@ -5,15 +5,15 @@ import javax.microedition.lcdui.Image;
 import javax.microedition.lcdui.game.Sprite;
 
 
-public class SpriteInicio extends Sprite{
+public class SpriteTrama extends Sprite{
 
 
-    public SpriteInicio(int x, int y) throws IOException {
-        super (Image.createImage("/imagenes/spriteInicio.png"),360, 50);
-        setPosition(0, 200);
+    public SpriteTrama(int x, int y) throws IOException {
+        super (Image.createImage("/imagenes/tramaSprite.png"),360, 50);
+        setPosition(0, 250);
         super.setFrame(0);
         //super.setFrameSequence(new int [] {0, 1});
-        
+
     }
 
     public void dibujar(Graphics g) {
@@ -21,5 +21,5 @@ public class SpriteInicio extends Sprite{
         super.paint(g);
     }
 
-   
+
 }
